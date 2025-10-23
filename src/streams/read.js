87442e42fd390f3592e8f50514eeb7fe4ca09e18
Muhammadamin <muhamadamin1992@gmail.com>
2025-10-23 20:52:59 +0500

    feat: implement streams read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,11 @@
+import { createReadStream } from "node:fs";
+
 const read = async () => {
-  // Write your code here
+  createReadStream(new URL("./files/fileToRead.txt", import.meta.url))
+    .on("data", (chunk) => {
+      process.stdout.write(chunk);
+    })
+    .on("end", () => process.stdout.write("\n"));
 };
 
 await read();
