5edba21c73b0195ca455882ae888107c1b74529e
Muhammadamin <muhamadamin1992@gmail.com>
2025-10-26 19:51:32 +0500

    feat: implement cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import { fork } from "node:child_process";
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const child = fork(new URL("./files/script.js", import.meta.url), [...args], {
+    stdio: ["pipe", "pipe", "ipc"],
+  });
+  child.stdout.setEncoding("utf-8");
+  child.stdout.on("data", (data) => {
+    process.stdout.write(data);
+  });
+  process.stdin.on("data", (data) => child.stdin.write(data));
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["1", "2"]);
